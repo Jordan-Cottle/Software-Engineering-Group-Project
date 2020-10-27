f7dd00da7c78a1bcb49c62a94a083a4be1f03a4f
Jordan Cottle <jordancottle622@gmail.com>
2020-10-27 16:58:38 -0400

    Add test for individual note page
    Fix missing data in note page

--- tests/test_server.py
+++ tests/test_server.py
@@ -17,20 +17,35 @@ def assert_nav_exists(page_data):
 
 def test_main_page(client):
     response = client.get("/")
+    html = response.data
 
-    assert_nav_exists(response.data)
+    assert_nav_exists(html)
 
     # Assert basic content is there
-    assert b"<h1>Welcome to the 49er Notes App!</h1>" in response.data
-    assert b"<h2>Use this site to maintain and organize your notes.</h2>" in response.data
+    assert b"<h1>Welcome to the 49er Notes App!</h1>" in html
+    assert b"<h2>Use this site to maintain and organize your notes.</h2>" in html
 
 def test_notes_page(client):
     response = client.get("/notes")
+    html = response.data
 
     print(response.data)
-    assert_nav_exists(response.data)
+    assert_nav_exists(html)
 
-    assert b"<h1>List of available notes</h1>" in response.data
+    assert b"<h1>List of available notes</h1>" in html
 
     for note in NOTES:
-        assert f'<li><a href="/notes/{note["id"]}">{note["title"]}</a></li>'.encode() in response.data
+        assert f'<li><a href="/notes/{note["id"]}">{note["title"]}</a></li>'.encode() in html
+
+def test_note_page(client):
+    note = NOTES[0]
+    response = client.get(f"/notes/{note['id']}")
+    html = response.data
+
+    assert_nav_exists(html)
+
+    assert b"<h1>Note Data</h1>" in html
+
+    assert note["title"].encode() in html
+    assert note["text"].encode() in html
+    assert note["date"].encode() in html
