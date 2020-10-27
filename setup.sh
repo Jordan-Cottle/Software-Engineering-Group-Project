868c34eaa41c3efef2cdefe852ccfa37560213dc
Jordan Cottle <jordancottle622@gmail.com>
2020-10-27 16:42:06 -0400

    Update setup to generate .pth file

--- setup.sh
+++ setup.sh
@@ -1,13 +1,31 @@
-python -m venv .venv
+python3 -m venv .venv
 
 ls .venv/bin 2>/dev/null
 
 if [[ $? -eq 2 ]]
-then  # Windows
-    source .venv/Scripts/activate
-else  # Not windows
-    source .venv/bin/activate
+then
+    windows=true
+else
+    windows=false
 fi
+
+echo $windows
+
+note_14="$(pwd)/note_14"
+if [[ $windows == "true" ]]
+then
+    echo "Activating windows virtual environment"
+    activate=.venv/Scripts/activate
+    include=".venv/Lib/site-packages/include.pth"
+else  # Not windows
+    echo "Activating linux virtual environment"
+    activate=.venv/bin/activate
+    include="$(find -name site-packages)/include.pth"
+fi
+
+source $activate
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+echo $note_14 > $include
