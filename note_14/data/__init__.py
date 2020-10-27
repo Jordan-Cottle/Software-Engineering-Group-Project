f5a80eac6659de7caaaa37d9c59236eb147e8fac
Jordan Cottle <jordancottle622@gmail.com>
2020-10-26 23:11:16 -0400

    Add simple data for testing

---
+++ note_14/data/__init__.py
@@ -0,0 +1,4 @@
+import json
+
+with open(r"C:\Users\Jordan\Documents\School\FALL 2020\ITSC 3155 Software Engineering\Software-Engineering-Group-Project\note_14\data\notes", "r") as note_file:
+    NOTES = json.load(note_file)
