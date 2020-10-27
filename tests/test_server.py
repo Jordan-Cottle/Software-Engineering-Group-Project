a0b3c9c518a8ced914a007aed236d93b20961aa1
Jordan Cottle <jordancottle622@gmail.com>
2020-10-27 16:52:20 -0400

    Add test for notes page
    Also fix bug the test found

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,7 @@
 import pytest
 
 from server import app
+from data import NOTES
 
 
 @pytest.fixture(name="client")
@@ -22,3 +23,14 @@ def test_main_page(client):
     # Assert basic content is there
     assert b"<h1>Welcome to the 49er Notes App!</h1>" in response.data
     assert b"<h2>Use this site to maintain and organize your notes.</h2>" in response.data
+
+def test_notes_page(client):
+    response = client.get("/notes")
+
+    print(response.data)
+    assert_nav_exists(response.data)
+
+    assert b"<h1>List of available notes</h1>" in response.data
+
+    for note in NOTES:
+        assert f'<li><a href="/notes/{note["id"]}">{note["title"]}</a></li>'.encode() in response.data
