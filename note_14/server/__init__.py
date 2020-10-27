03197b78b075feab4f7d12b67c2acb05c7d83b81
Jordan Cottle <jordancottle622@gmail.com>
2020-10-27 16:40:55 -0400

    Fix static and templates directories

--- note_14/server/__init__.py
+++ note_14/server/__init__.py
@@ -2,10 +2,16 @@
 
 The flask application object is created here and is available for import at the package level.
 """
-
 # pylint: disable=wrong-import-position
+
+import os
+
 from flask import Flask
 
-app = Flask("note_14")
+server_dir = os.path.dirname(__file__)
+statics = os.path.join(os.path.split(server_dir)[0], "static")
+templates = os.path.join(os.path.split(server_dir)[0], "templates")
+
+app = Flask("note_14", static_folder=statics, template_folder=templates)
 
 from .controller import *
