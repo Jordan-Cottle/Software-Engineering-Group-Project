5db21ee58396151cae7f4b0725a7f22236925856
Jordan Cottle <jordancottle622@gmail.com>
2020-10-27 15:11:56 -0400

    Appease pylint

--- note_14/app.py
+++ note_14/app.py
@@ -1,8 +1,12 @@
+""" Main module for actually running the flask application """
+
 import os
 
 from server import app
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 5000)), debug=True
+        host=os.getenv("IP", "127.0.0.1"),
+        port=int(os.getenv("PORT", "5000")),
+        debug=True,
     )
--- note_14/server/__init__.py
+++ note_14/server/__init__.py
@@ -1,3 +1,9 @@
+""" Initialization file for the server package.
+
+The flask application object is created here and is available for import at the package level.
+"""
+
+# pylint: disable=wrong-import-position
 from flask import Flask
 
 app = Flask("note_14")
