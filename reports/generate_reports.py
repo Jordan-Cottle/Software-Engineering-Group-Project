0122755f336d524ea01082e1ae43bb3d4fcac4c8
Jordan Cottle <jordancottle622@gmail.com>
2020-11-01 19:00:36 -0500

    parametrize report generation

--- reports/generate_reports.py
+++ reports/generate_reports.py
@@ -18,14 +18,22 @@ ONE_WEEK = timedelta(days=7)
 
 END_OF_SEMESTER = date(month=12, day=15, year=2020)
 
-report_date = date(month=11, day=1, year=2020)
-while report_date <= END_OF_SEMESTER:
+
+def new_report(report_date):
     for member in TEAM_MEMBERS:
         with open(
             f"{member}/{report_date.strftime(FILE_NAME_FORMAT)}.md", "w"
         ) as report_file:
             data = template.format(date=report_date.strftime(DATE_FORMAT))
             report_file.write(data)
-            # print(data)
 
-    report_date += ONE_WEEK
+
+def generate_new_reports(start=date.today(), end=END_OF_SEMESTER, step=ONE_WEEK):
+    report_date = start
+    while report_date <= end:
+        new_report(report_date)
+        report_date += step
+
+
+if __name__ == "__main__":
+    generate_new_reports()
