d81ecf0dde704d6e8b3a2632deaa3ea29a86227d
Jordan Cottle <jordancottle622@gmail.com>
2020-11-01 20:28:47 -0500

    Add ability to automatically merge individual reports

--- reports/generate_reports.py
+++ reports/generate_reports.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import date, timedelta
 
 TEAM_MEMBERS = [
@@ -35,5 +36,52 @@ def generate_new_reports(start=date.today(), end=END_OF_SEMESTER, step=ONE_WEEK)
         report_date += step
 
 
+SUB_TITLE = re.compile(r"#{2,}(.+)")
+LIST_ITEM = re.compile(r"\*.+")
+
+
+def parse_text(data):
+    items = {}
+
+    for line in data:
+        match = re.match(SUB_TITLE, line)
+        if match:
+            current_heading = match.group(1).strip()
+            items[current_heading] = []
+            continue
+
+        match = re.match(LIST_ITEM, line)
+        if match:
+            items[current_heading].append(line)
+
+    return items
+
+
+def generate_team_report(report_date):
+    data = {}
+    for member in TEAM_MEMBERS:
+        file_name = f"{member}/{report_date.strftime(FILE_NAME_FORMAT)}.md"
+        with open(file_name, "r") as individual_report:
+            data[member] = parse_text(individual_report.readlines())
+
+    team_report = []
+    for line in template.split("\n"):
+        section_heading = re.match(SUB_TITLE, line)
+        if "{date}" in line:
+            team_report.append(line.format(date=report_date.strftime(DATE_FORMAT)))
+        elif section_heading:
+            team_report.append(line)
+            for member, sections in data.items():
+                team_report.append(f"* {member.capitalize()}")
+                for item in sections[section_heading.group(1).strip()]:
+                    team_report.append(f"    {item}")
+
+    with open(f"team/{report_date.strftime(FILE_NAME_FORMAT)}.md", "w") as report_file:
+        print("\n".join(team_report), file=report_file)
+
+        print("## Meeting Agenda", file=report_file)
+        print("* ", file=report_file)
+
+
 if __name__ == "__main__":
-    generate_new_reports()
+    generate_team_report(date.today())
