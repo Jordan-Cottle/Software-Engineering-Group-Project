8d14e6fcf5c0356d00b79e0d98f06d136d5bba8e
Jordan Cottle <jordancottle622@gmail.com>
2020-11-12 18:58:12 -0500

    Set up database core

---
+++ note_14/database/__init__.py
@@ -0,0 +1,10 @@
+""" Package for providing database access to the application. """
+# pylint: disable=wrong-import-position
+
+from sqlalchemy import create_engine
+
+
+DB_FILENAME = "note14.db"
+ENGINE = create_engine(f"sqlite:///{DB_FILENAME}", echo=True)
+
+from .session import Session, inject_session, close_session
