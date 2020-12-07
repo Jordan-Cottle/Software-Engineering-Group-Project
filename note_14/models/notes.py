20aeab061c1d0713cd9852bad858587b9813f3c0
Jacob Woodhouse <jwoodhou@uncc.edu>
2020-12-07 16:32:36 -0500

    Fixed formatting issue

--- note_14/models/notes.py
+++ note_14/models/notes.py
@@ -7,10 +7,9 @@ NoteSection -- Represents a single section of a note.
 
 Rating -- Represents the rating of a single note
 """
-
+import datetime
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, LargeBinary, DateTime
 from sqlalchemy.orm import relationship
-import datetime
 from models import Base
 
 
