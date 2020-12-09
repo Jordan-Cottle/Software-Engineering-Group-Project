be5e5f4a2c1f36521591c356f93ce7d36755ebb6
tradefrosty <52793653+tradefrosty@users.noreply.github.com>
2020-12-08 21:03:12 -0500

    Add backref and cascade to user

--- note_14/models/users.py
+++ note_14/models/users.py
@@ -18,7 +18,13 @@ class User(Base):
     password = Column("password", String)
     salt = Column("salt", String)
 
-    notes = relationship("Note", order_by="Note.id", backref="owner")
+    notes = relationship(
+        "Note", order_by="Note.id", backref="owner", cascade="all, delete-orphan"
+    )
+    comments = relationship("Comment", backref="owner", cascade="all, delete-orphan")
+    attachments = relationship(
+        "Attachment", backref="owner", cascade="all, delete-orphan"
+    )
 
     def __str__(self):
         return f"User {self.name}"
