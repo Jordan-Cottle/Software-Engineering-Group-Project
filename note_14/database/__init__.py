1b56dc38e1c0ee8120dcb0f082675ab95177cd0a
Jordan Cottle <jordancottle622@gmail.com>
2020-12-10 04:01:44 -0500

    Check permissions when working with notes

--- note_14/database/__init__.py
+++ note_14/database/__init__.py
@@ -9,7 +9,12 @@ ENGINE = create_engine(f"sqlite:///{DB_FILENAME}", echo=ECHO)
 
 from .session import Session, inject_session, close_session
 
-from .permissions import add_permission, remove_permission
+from .permissions import (
+    add_permission,
+    remove_permission,
+    has_permission,
+    UnauthorizedError,
+)
 from .users import create_user, get_user, UserNotFound
 from .notes import get_note, get_notes, create_note, delete_note, edit_note
 from .ratings import create_rating
