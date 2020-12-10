662d3f332861be855b9325a07cb72e6e05d0d177
Jordan Cottle <jordancottle622@gmail.com>
2020-12-10 03:27:12 -0500

    Add login feature test

--- tests/features/sign_in.feature
+++ tests/features/sign_in.feature
@@ -3,11 +3,28 @@ Feature: User Sign In
     As a user
     I want to be able to sign in to my account
 
-    Scenario: Joe Signs in to his account
-        Given I am on the "login" page
-        And an account "Joe" with password "abdcefg" exists
-        And I enter "Joe" as my name
-        And I enter "abcdefg" as my password
-        When I click "login"
-        Then I should be logged in to the system
-        And I should see "Welcome back, Joe"
+    Scenario: Joe navigates to the login page
+        When I navigate to "/login"
+        Then I should see "Username"
+        And I should see "Password"
+        And I should see "Login"
+
+    Scenario: Joe signs in to his account
+        Given an account "Joe" with password "abcdefg" exists
+        When I navigate to "/login"
+        And I enter "Joe" for "user_name"
+        And I enter "abcdefg" for "password"
+        And I submit the form
+        Then I should be logged in as "Joe"
+        Then I should be redirected to "/"
+        And I should see "Welcome back to the 49er Notes App, Joe!"
+    
+    Scenario: Joe fails to sign into his account
+        Given an account "Joe" with password "abcdefg" exists
+        When I navigate to "/login"
+        And I enter "Joe" for "user_name"
+        And I enter "12345" for "password"
+        And I submit the form
+        Then I should be redirected to "/login?error=401"
+        And I should see "Your username or password was incorrect, please try again"
+
--- tests/features/step_defs/conftest.py
+++ tests/features/step_defs/conftest.py
@@ -1,7 +1,10 @@
 """ This module contains common fixtures and setup for bdd style tests. """
+import re
 
+import pytest
 from pytest_bdd import given, when, then
 from pytest_bdd.parsers import parse
+
 
 from database import create_user, create_note, get_user
 from models import User, Note
@@ -27,6 +30,8 @@ class TestSession:
         self.response = None
         self.user = None
 
+        self.form = {}
+
     def login(self, user):
         """ Log in a user. """
 
@@ -46,6 +51,12 @@ class TestSession:
         return self.response
 
 
+@pytest.fixture(name="test_session")
+def test_session(client):
+    """ Set up a TestSession to hold state between steps. """
+    return TestSession(client)
+
+
 @given(parse('a user "{user_name}" exists'))
 def user_exists(session, user_name):
     user = create_user(session, user_name, TEST_PASSWORD)
@@ -54,12 +65,10 @@ def user_exists(session, user_name):
     return user
 
 
-@given(parse('I am logged in as "{user_name}"'), target_fixture="test_session")
-def login_user(session, client, user_name):
-    print(*session.query(User).all())
+@given(parse('I am logged in as "{user_name}"'))
+def login_user(session, test_session, user_name):
     user = get_user(session, user_name)
 
-    test_session = TestSession(client)
     test_session.login(user)
 
     return test_session
@@ -88,15 +97,53 @@ def request_route(session, test_session, note_title):
     test_session.get(f"notes/{note.id}")
 
 
+@when(parse('I enter "{value}" for "{field}"'))
+def enter_form_data(test_session, value, field):
+    test_session.form[field] = value
+
+
+@when(parse("I submit the form"))
+def enter_form_data(test_session):
+    """ Detect form action and post data to it. """
+
+    html = test_session.response.get_data(as_text=True)
+    match = re.search(r'<form action="(/[^"]+)"', html)
+    assert match, "Form must be located on the html page in order to submit it!"
+
+    action = match.group(1)
+    print(f'Form action: "{action}"')
+
+    test_session.post(action, data=test_session.form)
+
+
 @then(parse('I should see "{text}"'))
 def check_response(test_session, text):
     data = test_session.response.get_data(as_text=True)
 
-    assert text in data, f"Response should contain {text}"
+    assert text in data, f"Response should contain {text}."
 
 
 @then(parse('I should not see "{text}"'))
 def check_response(test_session, text):
     data = test_session.response.get_data(as_text=True)
 
-    assert text not in data, f"Response should not contain {text}"
+    assert text not in data, f"Response should not contain {text}. {data}"
+
+
+@then(parse('I should be redirected to "{url}"'))
+def check_response(test_session, url):
+    """ Assert an expected redirect and follow it. """
+
+    html = test_session.response.get_data(as_text=True)
+
+    assert "Redirecting..." in html, "Request should have triggered a redirect!"
+    match = re.search(r'href="([^"]+)"', html)
+    assert match, f"{html} should have a redirect url in it!"
+
+    redirect_path = match.group(1)
+    assert (
+        redirect_path == url
+    ), f"Test session should be redirected to {url}, found {redirect_path} instead"
+
+    # Follow redirect for later steps to use
+    test_session.get(redirect_path)
