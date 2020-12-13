c91cee1d006e0478af6ab3bd632e9abdb903e73d
tradefrosty <52793653+tradefrosty@users.noreply.github.com>
2020-12-12 23:36:00 -0500

    Added get attachment and delete attachment features. (#178)
    * Added get attachment and delete attachment features.
    
    * Fixed formatting issue
    
    * Fixed formatting issue
    
    * Fixed formatting issue
    
    * Fixed formatting issue
    
    * I hate formatting tests
    
    * more fixes
    
    * Added fixture for attachment tests

--- note_14/database/notes.py
+++ note_14/database/notes.py
@@ -124,3 +124,19 @@ def add_attachment(session, attachment, note, user):
     attachment.save(file_name)
 
     return attachment_model
+
+
+def get_attachment(session, attachment_id, note, user):
+    """ Retrieves attachment from database """
+    check_permission(session, PermissionType.READ, user, note)
+    attachment = session.query(Attachment).filter_by(id=attachment_id).one()
+
+    return attachment
+
+
+def delete_attachment(session, attachment_id, note, user):
+    """ Deletes attachment from database """
+
+    check_permission(session, PermissionType.EDIT, user, note)
+    attachment = get_attachment(session, attachment_id, note, user)
+    session.delete(attachment)
